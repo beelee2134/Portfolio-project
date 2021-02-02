c158ed7e5a216a612ae2e74117b6df2078c7d56b
Beethoven <beelee2134@yahoo.com>
2021-02-01 23:24:12 -0800

    Blog update summary to body

--- Blog/models.py
+++ Blog/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=20)
     pub_date = models.DateTimeField()
-    summary = models.TextField()
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
